e23d9be4afbbc38ead0bdf66dd29319db40ed409
Fatema Kabir <fatema.kabir@pursuit.org>
2022-02-11 16:24:16 -0500

    snack.js-snacks.js-home.js

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,3 +1,12 @@
-const Home = () => {};
+import Snacks from "../Components/Snacks";
+
+const Home = () => {
+  return (
+    <div className="Index">
+      <h2>Get Snack'n at Snack-a-log!</h2>
+      <Snacks />
+    </div>
+  );
+};
 
 export default Home;
