881b8689d354e0eeda86239a5ed35a1f7fd7805c
BennyZ3 <bennyz3@hotmail.com>
2022-02-11 16:28:31 -0500

    finished form, and edit page

--- front-end/src/Components/Form.js
+++ front-end/src/Components/Form.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { request } from "express";
+import axios from "axios";
 
 const Form = (isEdit = false) => {
   let navigate = useNavigate();
@@ -14,17 +15,17 @@ const Form = (isEdit = false) => {
     is_healthy: false,
     image: "",
   });
-  if (isEdit) {
-    useEffect(() => {
+  useEffect(() => {
+    if (isEdit) {
       axios.get(`${API}/snacks/${id}`).then((response) => {
         setSnack(response.data);
       });
-    }, [API, id]);
-  }
+    }
+  }, [API, id]);
 
   const handleChange = (event) => {
     if (event.target.id === "is_healthy") {
-      setSnack({ ...snack, is_healthy: !snacks.is_healthy });
+      setSnack({ ...snack, is_healthy: !snack.is_healthy });
     } else {
       setSnack({ ...snack, [event.target.id]: event.target.value });
     }
@@ -50,5 +51,58 @@ const Form = (isEdit = false) => {
     }
   };
 
-  return <form></form>;
+  return (
+    <form onSubmit={handleSubmit}>
+      <label for="name">Name:</label>
+      <input
+        id="name"
+        name="name"
+        value={snack.name}
+        onChange={handleChange}
+        placeholder="name"
+      />
+      <label for="fiber">Protein:</label>
+      <input
+        id="fiber"
+        name="fiber"
+        value={snack.fiber}
+        onChange={handleChange}
+        placeholder="fiber"
+      />
+      <label for="protein">Protein:</label>
+      <input
+        id="protein"
+        name="protein"
+        value={snack.protein}
+        onChange={handleChange}
+        placeholder="protein"
+      />
+      <label for="added_sugar">Added Sugars:</label>
+      <input
+        id="added_sugar"
+        name="added_sugar"
+        value={snack.added_sugar}
+        onChange={handleChange}
+        placeholder="added_sugar"
+      />
+      <label for="is_healthy">Is it Healthy:</label>
+      <input
+        id="is_healthy"
+        name="is_healthy"
+        value={snack.is_healthy}
+        onChange={handleChange}
+        placeholder="is_healthy"
+      />
+      <label for="image">Image:</label>
+      <input
+        id="image"
+        name="image"
+        value={snack.image}
+        onChange={handleChange}
+        placeholder="image"
+      />
+    </form>
+  );
 };
+
+export default Form;
