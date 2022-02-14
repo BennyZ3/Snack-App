730d1ee2e0b9f4af99ddbf04f54678566dbb62b2
andres ballares <andr3sb@hotmail.com>
2022-02-14 11:19:45 -0500

    added Details Page

--- front-end/src/Pages/Details.js
+++ front-end/src/Pages/Details.js
@@ -1,3 +1,13 @@
-const Details = () => {};
+import React from "react";
+import Snack from "../Components/Snack";
+
+const Details = () => {
+    return (
+        <div>
+            <h2>Nutritional Facts</h2>
+            <Snack />
+        </div>
+    )
+};
 
 export default Details;
