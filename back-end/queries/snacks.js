9ab5942d271eb1971f597bbd2a984c9828b3e873
BennyZ3 <bennyz3@hotmail.com>
2022-02-15 12:56:25 -0500

    get and post tests

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -18,10 +18,29 @@ const getSnack = async (id) => {
   }
 };
 
+const capitalize = (words) => {
+  words = words.split(" ");
+  let result = [];
+  if (words.length > 1) {
+    words.forEach((word) => {
+      let letter = word.length > 2 ? word[0].toUpperCase() : word[0];
+      result.push(letter + word.slice(1).toLowerCase());
+    });
+  } else {
+    const letter =
+      words[0].length > 2 ? words[0][0].toUpperCase() : words[0][0];
+    result.push(letter + words[0].slice(1).toLowerCase());
+  }
+  return result.join(" ");
+};
+
 const createSnack = async (snack) => {
-  // console.log(snack);
   try {
-    const { name, fiber, protein, added_sugar, is_healthy, image } = snack;
+    let { name, fiber, protein, added_sugar, is_healthy, image } = snack;
+    name = capitalize(name);
+    if (!image) {
+      image = "https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image";
+    }
     const newSnack = await db.one(
       "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING * ",
       [name, fiber, protein, added_sugar, is_healthy, image]
@@ -46,10 +65,11 @@ const deleteSnack = async (id) => {
 
 const updateSnack = async (id, snack) => {
   try {
+    console.log(snack);
     const { name, fiber, protein, added_sugar, is_healthy, image } = snack;
     const updatedSnack = await db.one(
       "UPDATE snacks SET name=$2, fiber=$3, protein=$4, added_sugar=$5, is_healthy=$6, image=$7 WHERE id=$1 RETURNING *",
-      [name, fiber, protein, added_sugar, is_healthy, image]
+      [id, name, fiber, protein, added_sugar, is_healthy, image]
     );
     return updatedSnack;
   } catch (error) {
